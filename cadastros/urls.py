a75bb04ad993976d40856afca9fc5bfae3bfd15f
Rodrigo <rjr89000@gmail.com>
2022-03-14 15:07:28 -0300

    Cadastra e Remove cidades

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 
-from cadastros.views import lista_cidades, detalhe_cidade
+from cadastros.views import lista_cidades, detalhe_cidade, cadastra_cidade, remove_cidade
 
 urlpatterns = [
 
     path('', lista_cidades, name='cidades-list'),
-    path('detail/<int:id>/', detalhe_cidade, name='cidades-detalhe')
-
+    path('detail/<int:id>/', detalhe_cidade, name='cidades-detalhe'),
+    path('delete/<int:id>/', remove_cidade, name='cidades-remove'),
+    path('create/', cadastra_cidade, name='cadastra-cidade')
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from cadastros.forms import CidadeForm
 from cadastros.models import Cidade
 
 
 def lista_cidades(request):
 
-    qs = Cidade.objects.all()
+    qs = Cidade.objects.all().order_by('nome')
 
     context = {
         'cidades': qs,
@@ -26,3 +27,29 @@ def detalhe_cidade(request, id):
     }
 
     return render(request, 'cadastros/detalhe_cidades.html', context)
+
+def remove_cidade(request, id):
+
+    cidade = get_object_or_404(Cidade, pk=id)
+
+    cidade.delete()
+
+    return redirect('cidades-list')
+
+def cadastra_cidade(request):
+    if request.method == 'POST':
+
+            form = CidadeForm(request.POST)
+            if form.is_valid():
+
+                form.save()
+
+                return redirect('cidades-list')
+
+    else:
+        form = CidadeForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'cadastros/cadastra_cidades.html', context)
