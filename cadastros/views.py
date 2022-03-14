18c98627b7f2aa35b2711b434424ed292d43bc56
Rodrigo <rjr89000@gmail.com>
2022-03-14 11:22:41 -0300

    Lista e detalhe de cidades

--- cadastros/views.py
+++ cadastros/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from cadastros.models import Cidade
@@ -7,12 +7,22 @@ from cadastros.models import Cidade
 def lista_cidades(request):
 
     qs = Cidade.objects.all()
-    qs_capitais = Cidade.objects.filter(capital=True)
 
-    '''
-        query = 'SELECT * FROM cidade'
-        query = 'SELECT * FROM cidade WERE capital = TRUE'
-        cursor = db.connect().execute(query)
-        data = cursor.fetch()
-    
-    '''
+    context = {
+        'cidades': qs,
+        'titulo': 'SIDIA'
+    }
+    return render(request, 'cadastros/lista_cidades.html', context)
+
+def detalhe_cidade(request, id):
+
+    id_cidade = request.GET['id_cidade']
+    # cidade = Cidade.objects.all()
+    # cidade = Cidade.objects.get(pk=id_cidade)
+    cidade = get_object_or_404(Cidade, pk=id)
+
+    context = {
+        'cidade': cidade
+    }
+
+    return render(request, 'cadastros/detalhe_cidades.html', context)
